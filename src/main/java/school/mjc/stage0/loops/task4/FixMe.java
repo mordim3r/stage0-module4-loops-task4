64d2f019ef86401f7254944c54549928dfdbfb9d
mordim3r <valent1n.bazylev@gmail.com>
2026-06-07 12:55:04 +0300

    add

--- src/main/java/school/mjc/stage0/loops/task4/FixMe.java
+++ src/main/java/school/mjc/stage0/loops/task4/FixMe.java
@@ -3,12 +3,16 @@ package school.mjc.stage0.loops.task4;
 public class FixMe {
     public static void main(String[] args) {
         for (int i = 0; i < 100; i++) {
-            if (i % 2 == 0) {
-                System.out.println(i);
-            } else if (i == 7) {
-                System.out.println("last");
+            if (i == 8) {
                 break;
-                System.out.println("will I be printed?");
+            }
+            if (i % 2 == 0) {
+                System.out.print(i + " ");
+            }
+            if (i == 6) {
+                System.out.print("last ");
+                System.out.print("will I be printed?");
+                break;
             }
         }
     }
